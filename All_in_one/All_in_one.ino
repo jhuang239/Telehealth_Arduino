7f7dae7644ebed2b772784443f56cf43c2c30e05
Kim Wong <jk.wong1118@gmail.com>
2022-11-14 10:47:12 +0800

    first commit

---
+++ All_in_one/All_in_one.ino
@@ -0,0 +1,618 @@
+#include <ArduinoJson.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <dht.h>
+#include <MAX30100_PulseOximeter.h>
+
+#define OLED_I2C_ADDR 0x3C
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET -1 // SSD1306 doesn't have a reset pin
+
+#define dht_pin A7 //dht pin
+dht DHT;
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define REPORTING_PERIOD_MS     1000
+MAX30100 sensor;
+PulseOximeter pox;
+uint32_t tsLastReading = 0;
+
+int blood_pressure_high;
+int blood_pressure_low;
+float temperature;
+int blood_oxygen;
+float heartbeat;
+float room_temperature;
+float humidity;
+int ecgArr[128];
+
+String message = "";
+
+//blood_pressure monitor
+int val_hb = 0;// blood pressure signal
+int val_bp = 0;// average blood pressure
+
+//int blood_high;
+//int blood_low;
+int cnt = 0;
+
+//ecg
+int a = 0;
+int lasta = 0;
+int lastb = 0;
+
+void configureMax30100() {
+  sensor.setMode(MAX30100_MODE_SPO2_HR);
+  sensor.setLedsCurrent(MAX30100_LED_CURR_50MA, MAX30100_LED_CURR_27_1MA);
+  sensor.setLedsPulseWidth(MAX30100_SPC_PW_1600US_16BITS);
+  sensor.setSamplingRate(MAX30100_SAMPRATE_100HZ);
+  sensor.setHighresModeEnabled(true);
+}
+
+void read_DHT() {
+  oled_DHT();
+  room_temperature = 0;
+  humidity = 0;
+  int count = 0;
+  uint32_t starttime = millis();
+  while ((millis() - starttime) <= 30000) {
+    DHT.read11(dht_pin);
+    display.setCursor(74, 24);
+    display.print(DHT.temperature);
+    display.setCursor(70, 50);
+    display.print(DHT.humidity);
+    display.display();
+    room_temperature += DHT.temperature;
+    humidity += DHT.humidity;
+    count++;
+  }
+  room_temperature = room_temperature / count;
+  humidity = humidity / count;
+}
+
+
+void setupOLED(void) {
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDR)) { // Address 0x3C for SSD1306 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;) yield(); // Don't proceed, loop forever
+  }
+}
+
+void oled_blood_pressure() {
+  display.clearDisplay();
+  display.setTextSize(1); // Normal 1:1 pixel scale
+  display.setTextColor(WHITE, BLACK); // Draw white text
+
+  display.setCursor(0, 0);
+  display.cp437(true); // Use full 256 char 'Code Page 437' font
+  display.print("Blood Pressure Mon.");
+  display.setCursor(0, 16); // Start at top-left corner
+  display.print("RT_BP  :         mmHg");
+
+  display.setCursor(0, 32);
+  display.print("SYS_BP :         mmHg");
+
+  display.setCursor(0, 48);
+  display.print("DIA_BP :         mmHg");
+
+  display.display();
+  delay(2000);
+}
+
+void oled_welcome() {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE, BLACK);
+  display.setCursor(0, 0);
+  display.cp437(true);
+  display.println("Welcome to");
+  display.println("  Health");
+  display.println(" Companion");
+  display.display();
+  delay(2000);
+}
+
+void oled_DHT() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE, BLACK);
+  display.setCursor(0, 0);
+  display.cp437(true);
+  display.println("DHT measuring");
+  display.setCursor(0, 24);
+  display.print("Temperature:       C");
+  display.setCursor(0, 50);
+  display.print("Humidity:          %");
+  display.display();
+  delay(500);
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(115200);
+
+  pinMode(12, INPUT_PULLUP); //Button B record Diastolic pressure
+  pinMode(13, INPUT_PULLUP); //Button A -record Systolic pressure
+
+  //ecg
+  pinMode(9, INPUT); //+
+  pinMode(8, INPUT); //-
+
+  setupOLED();
+  oled_welcome();
+  //configureMax30100();
+}
+
+void temp_max30100_setup() {
+  if (!sensor.begin()) {
+    Serial.println("FAILED");
+    for (;;);
+  } else {
+    Serial.println("SUCCESS");
+  }
+  configureMax30100();
+}
+
+void hr_spo2_max30100_setup() {
+  if (!pox.begin()) {
+    Serial.println("FAILED");
+    for (;;);
+  } else {
+    Serial.println("SUCCESS");
+  }
+}
+
+void oled_ecg_instruction() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 20);
+  display.println("Instructions for ECG:");
+  display.setCursor(0, 30);
+  display.println("1. Attach the Red Pad to your Right Hand ");
+  display.display();
+  delay(3000);
+  display.clearDisplay();
+  display.display();
+  display.setCursor(0, 20);
+  display.println("2. Attach the Green  Pad to your Left Hand");
+  display.display();
+  delay(3000);
+  display.clearDisplay();
+  display.display();
+  display.setCursor(0, 20);
+  display.println("3.Attach the Yellow  Pad to your Right    Thigh.");
+  display.display();
+  delay(3000);
+  display.clearDisplay();
+  display.display();
+  display.setCursor(0, 20);
+  display.println("4.Press button B to capture the ECG graph when you feel stable");
+  display.display();
+  delay(3000);
+  display.clearDisplay();
+  display.display();
+  display.setCursor(0, 20);
+  display.println("5.If you understand  the instructions,   press button A to    start.");
+  display.display();
+}
+
+void oled_blood_pressure_instruction() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.clearDisplay();
+  display.setCursor(0, 20);
+  display.println("Instructions:");
+  display.setCursor(0, 30);
+  display.println("1.Press Air pump     Button.");
+  display.display();
+  delay(3000);
+  display.clearDisplay();
+  display.display();
+  display.setCursor(0, 20);
+  display.println("2.When the Real-Time pressure reaches 130mmHg,stop pressing Air  Pump Button.");
+  display.display();
+  delay(3000);
+  display.clearDisplay();
+  display.display();
+  display.setCursor(0, 20);
+  display.println("3.Wait the blood pressure to reduce until amplitude reaches maximum and turns steady");
+  display.display();
+  delay(3000);
+  display.clearDisplay();
+  display.display();
+  display.setCursor(0, 20);
+  display.println("4.Press Button A to  record the Systolic  Blood Pressure.");
+  display.display();
+  delay(3000);
+  display.clearDisplay();
+  display.display();
+  display.setCursor(0, 20);
+  display.println("5.Continue to observe the amplitude until it reduces to around half the the peak    value");
+  display.display();
+  delay(3000);
+  display.clearDisplay();
+  display.setCursor(0, 20);
+  display.println("6.Press Button B to  record the Diastolic Blood Pressure.");
+  display.display();
+  delay(3000);
+  display.clearDisplay();
+  display.setCursor(0, 20);
+  display.println("7.If you understand the instruction, Press button A to start");
+  display.display();
+}
+
+void oled_section(String section) {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 5);
+  if (section == "DHT") {
+    display.println("Measuring home temperature and humidity");
+    display.display();
+    delay(3000);
+  } else if (section == "MAX30100_temp") {
+    display.println("1.Place your finger on the sensor.");
+    display.print("2.Press button A to measure body temperature");
+    display.display();
+    delay(3000);
+  } else if (section == "MAX30100_hr") {
+    display.println("1.Place your finger on the sensor.");
+    display.print("2.Press button A to measure heart rate");
+    display.display();
+    delay(3000);
+  }
+  else {
+    display.println("1.Place your finger on the sensor.");
+    display.print("2.Press button A to measure spo2");
+    display.display();
+    delay(3000);
+  }
+}
+
+void oled_max30100_temp() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE, BLACK);
+  display.setCursor(0, 0);
+  display.cp437(true);
+  display.println("MAX30100 measuring");
+  display.setCursor(0, 40);
+  display.print("Temperature:       C");
+
+  delay(500);
+}
+
+void oled_max30100_spo2() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE, BLACK);
+  display.setCursor(0, 0);
+  display.cp437(true);
+  display.println("MAX30100 measuring");
+  //  display.setCursor(0, 20);
+  //  display.print("Heart rate:      bpm");
+  //  display.setCursor(0, 40);
+  //  display.print("Spo2:          %");
+  display.display();
+}
+
+void oled_max30100_hr() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE, BLACK);
+  display.setCursor(0, 0);
+  display.cp437(true);
+  display.println("MAX30100 measuring");
+  display.display();
+}
+
+void read_max30100_spo2() {
+  hr_spo2_max30100_setup();
+  oled_max30100_spo2();
+  blood_oxygen = 0;
+  uint32_t starttime = millis();
+  int count = 0;
+  while ((millis() - starttime) <= 30000) {
+    pox.update();
+    if (millis() - tsLastReading > REPORTING_PERIOD_MS) {
+      //      display.setCursor(70, 20);
+      //      display.print(pox.getHeartRate());
+      int value = pox.getSpO2();
+      display.setCursor(0, 40);
+      display.print("Spo2:          %");
+      display.setCursor(70, 40);
+      display.print(value);
+      display.display();
+      if (value <= 100 && value >= 90) {
+        blood_oxygen += value;
+        count++;
+      }
+      //      Serial.print("Heart rate:");
+      //      Serial.print(pox.getHeartRate());
+      Serial.print("SpO2:");
+      Serial.print(pox.getSpO2());
+      Serial.println("%");
+
+      tsLastReading = millis();
+    }
+  }
+  blood_oxygen = blood_oxygen / count;
+  pox.shutdown();
+}
+
+void read_max30100_hr() {
+  hr_spo2_max30100_setup();
+  oled_max30100_hr();
+  heartbeat = 0.0;
+  uint32_t starttime = millis();
+  int count = 0;
+  while ((millis() - starttime) <= 30000) {
+    pox.update();
+    if (millis() - tsLastReading > REPORTING_PERIOD_MS) {
+      float value = pox.getHeartRate();
+      heartbeat += value;
+      count++;
+      display.setCursor(0, 40);
+      display.print("Hear rate:       bpm");
+      display.setCursor(70, 40);
+      display.print(value);
+      display.display();
+      tsLastReading = millis();
+
+      Serial.print("BPM: ");
+      Serial.println(value);
+    }
+  }
+  heartbeat = heartbeat / count;
+  pox.shutdown();
+}
+
+void read_max30100_temp() {
+  temp_max30100_setup();
+  oled_max30100_temp();
+  temperature = 0.0;
+  uint32_t starttime = millis();
+  int count = 0;
+  while ((millis() - starttime) <= 30000) {
+    sensor.update();
+    if (millis() - tsLastReading > REPORTING_PERIOD_MS) {
+      sensor.startTemperatureSampling();
+      if (sensor.isTemperatureReady()) {
+        float temp = sensor.retrieveTemperature();
+        display.setCursor(74, 40);
+        display.print(temp);
+        display.display();
+        Serial.print("Temperature = ");
+        Serial.print(temp);
+        Serial.println("*C | ");
+
+        tsLastReading = millis();
+        if (temp > 35.00) {
+          temperature += temp;
+          count++;
+        }
+      }
+    }
+  }
+  temperature = temperature / count;
+  sensor.shutdown();
+}
+
+void unpack(String jsonMessage) {
+
+  //String usr_name = "testing";//testing without username
+  StaticJsonDocument<96> doc;
+  DeserializationError error = deserializeJson(doc, jsonMessage);
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+  String command = doc["command"];
+  String username = doc["username"];
+  if (command == "start_meausre") {
+
+    oled_section("DHT");
+    read_DHT();
+    Serial.print("Temperature: ");
+    Serial.println(room_temperature);
+    Serial.print("Humidity: ");
+    Serial.println(humidity);
+
+    oled_blood_pressure_instruction();
+    while (digitalRead(13) != LOW) {
+    }
+    blood_pressure();
+    Serial.print("bph: ");
+    Serial.println(blood_pressure_high);
+    Serial.print("bpl: ");
+    Serial.println(blood_pressure_low);
+
+    oled_section("MAX30100_temp");
+    while (digitalRead(13) != LOW) {
+    }
+    read_max30100_temp();
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+
+    oled_section("MAX30100_spo2");
+    while (digitalRead(13) != LOW) {
+    }
+    read_max30100_spo2();
+
+    oled_section("MAX30100_hr");
+    while (digitalRead(13) != LOW) {
+    }
+    read_max30100_hr();
+
+    Serial.print("heart rate: ");
+    Serial.print(heartbeat);
+    Serial.print("spo2: ");
+    Serial.print(blood_oxygen);
+
+    oled_ecg_instruction();
+    while (digitalRead(13) != LOW) {
+    }
+    ecg_measuremnt();
+
+    //    for (int i = 0; i < 128; i++) {
+    //      Serial.print(i + 1);
+    //      Serial.print(": ");
+    //      Serial.println(ecgArr[i]);
+    //    }
+    //display.clearDisplay();
+
+    String packJson = packData(username);
+    delay(3000);
+    Serial.println(packJson);
+    delay(3000);
+    Serial1.flush();
+    Serial1.println(packJson);
+    message = "";
+    //oled wellcome message
+    oled_welcome();
+  }
+}
+
+void ecg_measuremnt() {
+  display.clearDisplay();
+  int i = 0;
+  boolean clicked = false;
+  while (i < 128) {
+    if (digitalRead(12) == LOW) {
+      clicked = true;
+    }
+    if (a > 127)
+    {
+      display.clearDisplay();
+      a = 0;
+      lasta = a;
+    }
+//    while ((digitalRead(9) == 1) || (digitalRead(8) == 1)) {
+//      Serial.println('!');
+//    }
+    int value = analogRead(A5);
+    if (clicked) {
+      ecgArr[i] = value;
+      i++;
+    }
+    //Serial.println(value);
+    int b = 60 - (value / 16);
+    display.setCursor(0, 0);
+    display.writeLine(lasta, lastb, a, b, WHITE);
+    lastb = b;
+    lasta = a;
+    display.writeFillRect(0, 55, 128, 16, BLACK);
+    display.setCursor(0, 57);
+    display.print("Value:   ");
+    display.print(value);
+    display.display();
+    a++;
+    delay(100);
+  }
+}
+
+String packData(String username) {
+  Serial.println("packing");
+  String jsonMessage;
+
+  StaticJsonDocument<1536> obj;
+  obj["command"] = "upload";
+  obj["username"] = username;
+  obj["blood_pressure_high"] = blood_pressure_high;
+  obj["blood_pressure_low"] = blood_pressure_low;
+  obj["temperature"] = temperature;
+  obj["blood_oxygen"] = blood_oxygen;
+  obj["heartbeat"] = heartbeat;
+  obj["room_temperature"] = room_temperature;
+  obj["humidity"] = humidity;
+  JsonArray ecg = obj.createNestedArray("ecgData");
+  for (int i = 0; i < 128; i++) {
+    Serial.print("packing ecgData: ");
+    Serial.println(i + 1);
+    ecg.add(ecgArr[i]);
+  }
+  serializeJson(obj, jsonMessage);
+  return jsonMessage;
+}
+
+
+void loop() {
+  while (Serial1.available() > 0) {
+    message = Serial1.readString();
+    if (message.length() > 0) {
+      Serial.println(message);
+      unpack(message);
+    }
+  }
+}
+
+void blood_pressure() {
+  oled_blood_pressure();
+  blood_pressure_high = 0;
+  blood_pressure_low = 0;
+  while (blood_pressure_high == 0 || blood_pressure_low == 0) {
+    // measuare the voltage HB-o/p of Pump Control Circuit
+    val_hb = analogRead(A0);
+    //Serial.println(val_hb);
+    cnt = (cnt + 1) % 20;
+
+    // measure the voltage BP-o/p of the PSM
+    // and convert it into corresponding pressure in mmHg
+    // BP(mmHg) = 80.0064*DVBP*Vcc/1023 - 24.0019;
+    // DVBP: digital value of the pressure sensor output voltage sampled by analogRead
+    // Vcc: 5V
+    // The above formula is based on pressure sensor sensitivity chart
+    val_bp = int((80.0064 * 5 * analogRead(A1) / 1023) - 24.0019);
+
+    // avoid negative readings
+    if (val_bp <= 0) {
+      val_bp = 0;
+    }
+
+    // update realtime blood pressure every 10 samples interval
+    if ((cnt % 10) == 0) {
+      display.fillRect(50, 16, 45, 8, BLACK);
+      display.setTextColor(WHITE, BLACK); // Draw white text
+      display.setCursor(50, 16);
+      display.print(String(val_bp, DEC));
+      display.display();
+      Serial.print("current Blood pressure: ");
+      Serial.println(val_bp);
+    }
+
+    // record the current blood pressure as Sys Blood Pressure
+    // when Button A is pressed. Reading shows on OLED
+    int SYS_RB = digitalRead(13);
+
+    if (digitalRead(13) == LOW) {
+      display.fillRect(50, 32, 45, 8, BLACK);
+      display.setTextColor(WHITE, BLACK); // Draw white text
+      display.setCursor(50, 32);
+      display.print(String(val_bp, DEC));
+      display.display();
+      blood_pressure_high = val_bp;
+    }
+
+    // record the current blood pressure as Dia Blood Pressure
+    // when Button B is pressed. Reading shows on OLED
+    int DIA_RB = digitalRead(12);
+    if (digitalRead(12) == LOW) {
+      display.fillRect(50, 48, 45, 8, BLACK);
+      display.setTextColor(WHITE, BLACK); // Draw white text
+      display.setCursor(50, 48);
+      display.print(String(val_bp, DEC));
+      display.display();
+      blood_pressure_low = val_bp;
+    }
+    delay(20);
+  }
+}
---
+++ ESP32_MQTT/ESP32_MQTT.ino
@@ -0,0 +1,152 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+
+#define RXp2 16
+#define TXp2 17
+
+//const char* ssid = "EE3070_P1615_1";
+//const char* password = "EE3070P1615";
+const char* ssid = "Kim";
+const char* password = "Kim.W?0591";
+
+const char* root_ca = \
+                      "-----BEGIN CERTIFICATE-----\n" \
+                      "MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF\n" \
+                      "ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6\n" \
+                      "b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL\n" \
+                      "MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv\n" \
+                      "b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj\n" \
+                      "ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM\n" \
+                      "9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw\n" \
+                      "IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6\n" \
+                      "VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L\n" \
+                      "93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm\n" \
+                      "jgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC\n" \
+                      "AYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA\n" \
+                      "A4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI\n" \
+                      "U5PMCCjjmCXPI6T53iHTfIUJrU6adTrCC2qJeHZERxhlbI1Bjjt/msv0tadQ1wUs\n" \
+                      "N+gDS63pYaACbvXy8MWy7Vu33PqUXHeeE6V/Uq2V8viTO96LXFvKWlJbYK8U90vv\n" \
+                      "o/ufQJVtMVT8QtPHRh8jrdkPSHCa2XV4cdFyQzR1bldZwgJcJmApzyMZFo6IQ6XU\n" \
+                      "5MsI+yMRQ+hDKXJioaldXgjUkK642M4UwtBV8ob2xJNDd2ZhwLnoQdeXeGADbkpy\n" \
+                      "rqXRfboQnoZsG4q5WTP468SQvvG5\n" \
+                      "-----END CERTIFICATE-----\n";
+
+
+//MQTT
+const char* brokerUrl = "driver.cloudmqtt.com";
+int Port = 18712;
+const char* userMQTT = "cgxgibnr";
+const char* passMQTT = "MzxIi8_n_rr_";
+const char* subUser = "EE3070GP2/user";
+const char* outTopic = "EE3070GP2/outTopic";
+const String clienteId = "ESP32EE3070MQTT";
+uint16_t keepAlive = 600;
+
+WiFiClient espClient;
+PubSubClient clientMqtt(espClient);
+
+void reconnect() {
+  while (!clientMqtt.connected()) {
+    Serial.print("\nConnecting to ");
+    Serial.println(brokerUrl);
+    if (clientMqtt.connect(clienteId.c_str(), userMQTT, passMQTT)) {
+      Serial.print("\nConnected to ");
+      Serial.print(brokerUrl);
+      Serial.println();
+      clientMqtt.subscribe(subUser);
+      //      clientMqtt.subscribe(subCommand);
+    } else {
+      Serial.println(clientMqtt.state());
+      Serial.println("\nTrying connect again!");
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String s_topic = String(topic);
+  String message = "";
+  if (s_topic == String(subUser)) {
+    Serial.print("Received messages: ");
+    Serial.println(topic);
+    for (int i = 0; i < length; i++) {
+      message += String((char)payload[i]);
+    }
+    String jsonData = packJson(message);
+    Serial2.flush();
+    Serial2.println(jsonData);
+    String backMessage = Serial2.readString();
+    while (!unpackBackMessage(backMessage)) {
+      backMessage = Serial2.readString();
+    }
+    upload(backMessage);
+    delay(3000);
+    clientMqtt.publish(outTopic, "finished");
+  }
+}
+
+boolean unpackBackMessage(String jsonMessage) {
+  StaticJsonDocument<6144> doc;
+  DeserializationError error = deserializeJson(doc, jsonMessage);
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return false;
+  }
+  String command = doc["command"];
+  if (command == "upload") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void upload(String jsonData) {
+  if ((WiFi.status() == WL_CONNECTED) && jsonData.length() > 0) {
+    HTTPClient http;
+    http.begin("https://ee3070t2.herokuapp.com/addHealthData", root_ca);
+    http.addHeader("Content-Type", "application/json");
+    int httpCode = http.POST(jsonData);
+    if (httpCode > 0) {
+      String payload = http.getString();
+      Serial.println(payload);
+    }
+    else {
+      Serial.println("Error on HTTP request");
+    }
+    http.end();
+  }
+}
+
+String packJson(String username) {
+  String output;
+  StaticJsonDocument<64> doc;
+  doc["command"] = "start_meausre";
+  doc["username"] = username;
+  serializeJson(doc, output);
+
+  return output;
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial2.begin(115200, SERIAL_8N1, RXp2, TXp2);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+  clientMqtt.setServer(brokerUrl, Port);
+  clientMqtt.setCallback(callback);
+  clientMqtt.setKeepAlive(keepAlive);
+}
+
+void loop() {
+  if (!clientMqtt.connected()) {
+    reconnect();
+  }
+  clientMqtt.loop();
+}
